26e59c55dc0a82b890650211f6b0871b7ecbcdd8
rpundl <86139136+rpundl@users.noreply.github.com>
2023-02-14 23:08:18 -0500

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 CELERY_BROKER_URL = os.environ['REDIS_URL']
